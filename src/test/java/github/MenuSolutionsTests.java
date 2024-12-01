9fd5c9a78bb53adeba1166718211af693f998b3b
lyulol <lyulol.n@gmail.com>
2024-12-01 23:24:08 +0200

    TASK: added MenuSolutionsTests

---
+++ src/test/java/github/MenuSolutionsTests.java
@@ -0,0 +1,27 @@
+package github;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Configuration.baseUrl;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class MenuSolutionsTests {
+    @BeforeAll
+    static void beforeAll() {
+        baseUrl = "https://github.com";
+        Configuration.pageLoadStrategy = "eager";
+    }
+
+    @Test
+    void openEnterprisesPage() {
+        open(baseUrl);
+        $(".HeaderMenu-nav").$(byText("Solutions")).hover();
+        $(".HeaderMenu-item", 1).$(byText("Enterprises")).click();
+        $("#hero-section-brand-heading").shouldHave(text("The AI-powered developer platform"));
+    }
+}
